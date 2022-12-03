66b010e9c972d20f3992e02928c82454793b9985
Jiyong Youn <01@0101010101.com>
2022-12-03 18:21:52 +0900

    update usb device description

--- bootloader/F4/Src/usbd_desc.c
+++ bootloader/F4/Src/usbd_desc.c
@@ -95,10 +95,10 @@
 #define USBD_LANGID_STRING     				1033
 #define USBD_MANUFACTURER_STRING			"Jiyong Youn"
 #define USBD_PID_FS					48826  //BEBA hex
-#define USBD_PRODUCT_STRING_FS				"STM32 HID bootloader"
+#define USBD_PRODUCT_STRING_FS				"STM32 Pro Mini HID"
 #define USBD_SERIALNUMBER_STRING_FS			"1234567890"
-#define USBD_CONFIGURATION_STRING_FS			"STM32 HID bootloader Config"
-#define USBD_INTERFACE_STRING_FS			"STM32 HID bootloader Interface"
+#define USBD_CONFIGURATION_STRING_FS			"STM32 Pro Mini HID bootloader Config"
+#define USBD_INTERFACE_STRING_FS			"STM32 Pro Mini HID bootloader Interface"
 
 #define USB_SIZ_BOS_DESC            			0x0C
 
